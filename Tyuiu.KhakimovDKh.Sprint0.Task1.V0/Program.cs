01c33e44d240ab4ce1db8f34230debbde2ed35bf
Tyuiu.Khakimov_d_kh@outlook.com <manthreemanthree@gmail.com>
2024-12-11 18:47:49 +0500

    Task1

---
+++ Tyuiu.KhakimovDKh.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KhakimovDKh.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            string name = "Doston";
+            int age = 19;
+            Console.Write("Здравствуйте меня зовут " + name + " мне " + age + " лет");
+            Console.ReadKey();
+
+        }
+    }
+}
